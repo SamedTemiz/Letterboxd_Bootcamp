04a338174d7c3e953819db675dc1148b1006de42
SamedTemiz <samedtemiz99@gmail.com>
2024-08-26 20:07:39 +0300

    Colors added.

--- app/src/main/java/com/timrashard/letterboxd_bootcamp/ui/theme/Color.kt
+++ app/src/main/java/com/timrashard/letterboxd_bootcamp/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Letterboxd Theme Colors
+val Orange = Color(0xFFff8000)
+val Green = Color(0xFF00e054)
+val Blue = Color(0xFF40bcf4)
+val Bg_Black = Color(0xFF202830)
+val Bg_Gray = Color(0xFF303940)
+val Bg_LightGray = Color(0xFF445566)
+val Txt_White = Color(0xFFffffff)
